75f3393dca5bc56be231af47e4b165349e4b4b65
Miguel Quiroz <miguel.quiroz@contpaqi.com>
2020-02-12 09:50:44 -0600

    Cambio a messenger

--- CucumberJS/lib/messenger.js
+++ CucumberJS/lib/messenger.js
@@ -9,20 +9,57 @@ class Messenger {
     } 
 
     getMessageWithHour(hour, languaje) {
+        var result = ""; 
 
         if (hour >= "00:00" && hour < "12:00")
-        { 
-            return (languaje == undefined || languaje == "es") ?  "Buenos dias" :  "good morning";            
+        {
+            switch (languaje)  
+            {
+                case "es": 
+                    result = "Buenos dias"; 
+                break; 
+                case "us":  
+                    result = "good morning"; 
+                break;  
+                case "fr":  
+                    result = "Bonjour";
+                break;  
+            }
         }
         else if (hour >= "12:00" && hour < "19:00")
         {
-            return (languaje == undefined || languaje == "es") ? "Buenas tardes" : "Good afternoon";
+            switch (languaje)  
+            {
+                case "es": 
+                    result = "Buenas tardes"; 
+                break; 
+                case "us":  
+                    result = "Good afternoon"; 
+                break;  
+                case "fr":  
+                    result = "Bonsoir";
+                break;  
+            }
         }
         else if(hour >= "19:00")
         {
-            return (languaje == undefined || languaje == "es") ? "Buenas noches" : "Goodnight";
+            switch (languaje)  
+            {
+                case "es": 
+                    result = "Buenas noches"; 
+                break; 
+                case "us":  
+                    result = "Goodnight"; 
+                break;  
+                case "fr":  
+                    result = "Bonne nuit";
+                break;  
+            }
+
+            //return (languaje == undefined || languaje == "es") ? "Buenas noches" : "Goodnight";
         }
-        return "Hello World!";
+
+        return result; 
     }
 }
 
--- CucumberJS/test/test-messenger.js
+++ CucumberJS/test/test-messenger.js
@@ -16,49 +16,6 @@ describe('Messenger', function() {
         });
     });
  
-    describe ('Get a friendly day message ', function() {
-        it('return a good day message', function(done){
-
-            //Arrange
-            let mesenger;
-            mesenger = new Messenger( );
-            //Act
-            let message =  mesenger.getMessageWithHour(new Date(2020, 02, 11, 0, 0, 0).toTimeString(), "es");
-            //Assert
-            expect(message).to.equal("Buenos dias");
-             done();
-        });
-    });
-
-    describe ('Get a friendly day message ', function() {
-        it('return a good afternoon message', function(done){
-
-            //Arrange
-            let mesenger;
-            mesenger = new Messenger( );
-            //Act
-            let message =  mesenger.getMessageWithHour(new Date(2020, 02, 11, 12, 0, 0).toTimeString());
-            //Assert
-            expect(message).to.equal("Buenas tardes");
-             done();
-        });
-    });
-
-    describe ('Get a friendly day message ', function() {
-        it('return a Goodnight message', function(done){
-
-            //Arrange
-            let mesenger;
-            mesenger = new Messenger( );
-            //Act
-            let message =  mesenger.getMessageWithHour(new Date(2020, 02, 11, 19, 0, 0).toTimeString());
-            //Assert
-            expect(message).to.equal("Buenas noches");
-             done();
-        });
-    });
-    
-
     describe ('Get a friendly day message in spanish ', function() {
         it('return a good morning message', function(done){
 
@@ -87,6 +44,20 @@ describe('Messenger', function() {
         });
     });
 
+
+    describe ('Get a friendly day message in french ', function() {
+        it('return a good morning message set fr', function(done){
+
+            //Arrange
+            let mesenger;
+            mesenger = new Messenger( );
+            //Act
+            let message =  mesenger.getMessageWithHour(new Date(2020, 02, 11, 0, 0, 0).toTimeString(), "fr");
+            //Assert
+            expect(message).to.equal("Bonjour");
+             done();
+        });
+    });
 
 
 
@@ -119,7 +90,19 @@ describe('Messenger', function() {
     });
 
 
+    describe ('Get a friendly good afternoon message in french', function() {
+        it('return a good afternoon message in fr language', function(done){
 
+            //Arrange
+            let mesenger;
+            mesenger = new Messenger( );
+            //Act
+            let message =  mesenger.getMessageWithHour(new Date(2020, 02, 11, 12, 0, 0).toTimeString(), "fr");
+            //Assert
+            expect(message).to.equal("Bonsoir");
+             done();
+        });
+    });
 
     describe ('Get a friendly Goodnight message in spanish', function() {
         it('return a Goodnight message in spanish language', function(done){
@@ -149,6 +132,18 @@ describe('Messenger', function() {
         });
     });
 
+    describe ('Get a friendly Goodnight message in fr language', function() {
+        it('return a Goodnight message in fr language', function(done){
 
+            //Arrange
+            let mesenger;
+            mesenger = new Messenger( );
+            //Act
+            let message =  mesenger.getMessageWithHour(new Date(2020, 02, 11, 19, 0, 0).toTimeString(), "fr");
+            //Assert
+            expect(message).to.equal("Bonne nuit");
+             done();
+        });
+    });
 
 });
